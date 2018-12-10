f24cbbced6559a9c9e1e6f6f39629cdb940ade32
dangwuxie <974079012@qq.com>
2018-12-10 21:43:56 +0800

    修改代码

--- Dec_JAVA/继承的定义与使用/Test2.java
+++ Dec_JAVA/继承的定义与使用/Test2.java
@@ -23,6 +23,13 @@ class B extends A{
 }
 //7 8 9 3 6 s 2154 2154 e
 public class Test2{
+    
+    public static void main(String[] args){
+        System.out.println("start-----------------");
+        new B();
+        new B();
+        System.out.println("end-------------------");
+    }
     static{
         System.out.println("7、主类中的静态代码块");
     }
@@ -32,10 +39,17 @@ public class Test2{
     public Test2(){
         System.out.println("9、主类中的构造方法");
     }
-    public static void main(String[] args){
-        System.out.println("start-----------------");
-        new B();
-        new B();
-        System.out.println("end-------------------");
-    }
 }
+// 7、主类中的静态代码块
+// start-----------------
+// 3、父类的静态代码块块
+// 6、子类的静态代码块
+// 2、父类的构造块
+// 1、父类的构造函数
+// 5、子类的构造块
+// 4、子类的构造方法
+// 2、父类的构造块
+// 1、父类的构造函数
+// 5、子类的构造块
+// 4、子类的构造方法
+// end-------------------
